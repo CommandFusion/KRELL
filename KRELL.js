01fa98ca57ed32ee3ed65ea6a19968b9c0e8d000
Jarrod Bell <jarrod@commandfusion.com>
2014-09-17 09:33:45 +1000

    Initial commit of very simple features

---
+++ KRELL.js
@@ -0,0 +1,23 @@
+CF.userMain = function() {
+	// This is where we tell the code what feedback we want to process
+	CF.watch(CF.FeedbackMatchedEvent, "KRELL", "KRELL_FB", function(fbName, data) {
+		// The 'data' variable will contain all the data captured by the feedback as defined in your System, via System Manager
+		var byte4 = data.charCodeAt(3); // Get the 4th byte, 0-based (starts at 0)
+		var powerState = (byte4 & 1 != 0); // "AND" with 1 (binary 00000001)
+		var byte4bit2 = (byte4 & 2 ? 1 : 0);
+		var byte4bit3 = (byte4 & 4 ? 1 : 0);
+		var byte4bit4 = (byte4 & 8 ? 1 : 0);
+		var byte4bit5 = (byte4 & 16 ? 1 : 0);
+		var byte4bit6 = (byte4 & 32 ? 1 : 0);
+		var byte4bit7 = (byte4 & 64 ? 1 : 0);
+		var byte4bit8 = (byte4 & 128 ? 1 : 0);
+
+		var volumeLevel = data.charCodeAt(12) // Get the 13th byte, 0-based
+		CF.log("Power: " + powerState);
+		CF.log("Volume: " + volumeLevel);
+
+		CF.setJoin("d10", powerState);					// Set button state based on the power state, for button on digital join 10.
+		CF.setJoin("a2", (65535/255) * volumeLevel);	// Adjust slider on join 2 to show volume level.
+														// Sliders have a range of 0-65535, so we have to convert the value from byte range (0-255) first
+	});
+};
