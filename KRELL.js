3f8616c31917789585addeaedefb358899e0bfae
Jarrod Bell <jarrod@commandfusion.com>
2014-09-21 10:43:58 +1000

    Log all output

--- KRELL.js
+++ KRELL.js
@@ -1,6 +1,9 @@
 CF.userMain = function() {
 	// This is where we tell the code what feedback we want to process
 	CF.watch(CF.FeedbackMatchedEvent, "KRELL", "KRELL_FB", function(fbName, data) {
+		// Log the output of all incoming data in a readable format
+		CF.log(makeReadable(data));
+
 		// The 'data' variable will contain all the data captured by the feedback as defined in your System, via System Manager
 		var byte4 = data.charCodeAt(3); // Get the 4th byte, 0-based (starts at 0)
 		var powerState = (byte4 & 1 != 0); // "AND" with 1 (binary 00000001)
@@ -21,3 +24,16 @@ CF.userMain = function() {
 														// Sliders have a range of 0-65535, so we have to convert the value from byte range (0-255) first
 	});
 };
+
+function makeReadable(bytes) {
+    var readable = "", i;
+    for (i = 0; i < bytes.length; i++) {
+        var byteVal = bytes.charCodeAt(i);
+        if (byteVal < 32 || byteVal > 127) {
+            readable += "\\x" + ("0" + byteVal.toString(16).toUpperCase()).slice(-2);
+        } else {
+            readable += bytes[i];
+        }
+    }
+    return readable;
+}
